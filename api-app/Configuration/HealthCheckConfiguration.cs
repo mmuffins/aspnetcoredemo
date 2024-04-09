168f7d0e3e648b63b19ed5f447033a00d238ad5b
mmuffins <mail@tiv73.com>
2024-04-09 21:49:22 +0200

    add project files

---
+++ api-app/Configuration/HealthCheckConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace api_app.Configuration
+{
+    internal class HealthCheckConfiguration
+    {
+        internal static void Apply(WebApplicationBuilder builder)
+        {
+            //builder.Services.AddHealthChecks()
+            //    .AddCheck(
+            //    "self")
+        }
+    }
+}
---
+++ api-app/Controllers/ProbesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_app.Controllers
+{
+    [ApiController]
+    public class ProbesController : ControllerBase
+    {
+        [HttpGet("live")]
+        public IActionResult LivenessProbe()
+        {
+            // Always return OK for demo purposes
+            return Ok("Healthy");
+        }
+
+        [HttpGet("ready")]
+        public IActionResult ReadinessProbe()
+        {
+            // Always return OK for demo purposes
+            return Ok("Ready");
+        }
+    }
+}
